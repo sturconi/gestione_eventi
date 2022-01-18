298241be7cdb4811bbc37e26ba948b1b99d94ddb
FrancoCeruti <94625923+FrancoCeruti@users.noreply.github.com>
2022-01-18 16:23:17 +0100

    Aggiunti ToggleButton e cambiamento del layout Privacy

--- gestione_eventi_android/app/src/main/java/com/example/applicazionevera/Privacy.java
+++ gestione_eventi_android/app/src/main/java/com/example/applicazionevera/Privacy.java
@@ -2,8 +2,11 @@ package com.example.applicazionevera;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.CompoundButton;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class Privacy extends AppCompatActivity {
 
@@ -12,35 +15,34 @@ public class Privacy extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_privacy);
         Toast.makeText(this, "privacy", Toast.LENGTH_SHORT).show();
-    }
 
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-    }
+        ToggleButton ToggleGps = (ToggleButton) findViewById(R.id.ToggleGps);
+        ToggleButton ToggleNC = (ToggleButton) findViewById(R.id.ToggleNC);
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
+        ToggleGps.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (ToggleGps.isChecked() == true) {
+                    Toast.makeText(Privacy.this, "Gps Attivato!", Toast.LENGTH_LONG).show();
+                    ToggleGps.setTextColor(Color.GREEN);
+                } else {
+                    ToggleGps.setTextColor(Color.RED);
+                    Toast.makeText(Privacy.this, "Gps Disattivato!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
+        ToggleNC.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (ToggleNC.isChecked() == true) {
+                    Toast.makeText(Privacy.this, "Nome e cognome attivati!", Toast.LENGTH_LONG).show();
+                    ToggleNC.setTextColor(Color.GREEN);
+                } else {
+                    ToggleNC.setTextColor(Color.RED);
+                    Toast.makeText(Privacy.this, "Nome e cognome nascosti!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
     }
 }
