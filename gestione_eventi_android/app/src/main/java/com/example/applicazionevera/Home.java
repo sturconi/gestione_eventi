13ac247dd0e11843f172493373e68e5bf6c5e0fe
MatteoRomagnoni <94623792+MatteoRomagnoni@users.noreply.github.com>
2022-01-18 16:34:26 +0100

    Modifica passaggio tra schermata Login e schermata Home

--- gestione_eventi_android/app/src/main/java/com/example/applicazionevera/Home.java
+++ gestione_eventi_android/app/src/main/java/com/example/applicazionevera/Home.java
@@ -6,13 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
+
+    String username = null;
+    String password = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Bundle datiLogin = getIntent().getExtras();
+        if(datiLogin != null) {
+            username = datiLogin.getString("user");
+            password = datiLogin.getString("pwd");
+
+            if(username.length()>0 && password.length() >0)
+                Toast.makeText(this, "Benvenuto " + username, Toast.LENGTH_SHORT).show();
+        }
+
         Button button;
         button =(Button) findViewById(R.id.toCategorie);
         button.setOnClickListener(new View.OnClickListener() {
--- gestione_eventi_android/app/src/main/java/com/example/applicazionevera/Login.java
+++ gestione_eventi_android/app/src/main/java/com/example/applicazionevera/Login.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,22 +12,30 @@ import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
+    EditText userET = findViewById(R.id.username);
+    EditText passwordET = findViewById(R.id.username);
+
+    String username = userET.getText().toString();
+    String password = passwordET.getText().toString();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Button button;
-        EditText user = findViewById(R.id.username);
-        EditText password = findViewById(R.id.username);
+        userET = findViewById(R.id.username);
+        passwordET = findViewById(R.id.username);
 
 
         button =(Button) findViewById(R.id.buttonAccedi);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String userString = user.getText().toString();
-                if (userString.contains("@")) {
+
+                if (username.contains("@")) {
                 openHome();
+
                 }
                 else
                     Toast.makeText(Login.this, "@ mancante", Toast.LENGTH_SHORT).show();
@@ -43,8 +52,11 @@ public class Login extends AppCompatActivity {
     }
 
     public void openHome() {
-        Intent intent = new Intent(this, Home.class);
-        startActivity(intent);
+        Intent HomeIntent = new Intent(this, Home.class);
+
+        HomeIntent.putExtra("user", username);
+        HomeIntent.putExtra("pwd",  password);
+        startActivity(HomeIntent);
     }
 
     public void openRegistrazione() {
