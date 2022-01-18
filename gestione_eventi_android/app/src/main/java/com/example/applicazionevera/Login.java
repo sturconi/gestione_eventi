79be13c8617e3ccce1b6fa4c8a8cb06a4f7f0eaa
FrancoCeruti <94625923+FrancoCeruti@users.noreply.github.com>
2022-01-18 12:17:55 +0100

    Aggiunto SwitchButton in preferenza

--- gestione_eventi_android/app/src/main/java/com/example/applicazionevera/Login.java
+++ gestione_eventi_android/app/src/main/java/com/example/applicazionevera/Login.java
@@ -30,8 +30,6 @@ public class Login extends AppCompatActivity {
                 }
                 else
                     Toast.makeText(Login.this, "@ mancante", Toast.LENGTH_SHORT).show();
-
-
             }
         });
 
