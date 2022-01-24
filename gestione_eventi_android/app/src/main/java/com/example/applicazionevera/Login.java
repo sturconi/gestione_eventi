060ca424ad762436e1677b02ec47ca228f48f40e
Kristhen <k.mercoledisanto@itsrizzoli.it>
2022-01-24 16:59:22 +0100

    Aggiunto il SavePreferencesData ed il UpdatePreferencesdata

--- gestione_eventi_android/app/src/main/java/com/example/applicazionevera/Login.java
+++ gestione_eventi_android/app/src/main/java/com/example/applicazionevera/Login.java
@@ -2,12 +2,15 @@ package com.example.applicazionevera;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
@@ -17,7 +20,10 @@ public class Login extends AppCompatActivity {
 
     String username =  null;
     String password = null ;
+    private  final  static  String  MY_PREFERENCES  =  "MyPref";
 
+    private  final  static  String  TEXT_LOGIN_KEY  =  "textLogin";
+    private  final  static  String  TEXT_PASSWORD_KEY  =  "textPassword";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,11 @@ public class Login extends AppCompatActivity {
         Button button;
         userET = findViewById(R.id.username);
         passwordET = findViewById(R.id.password);
+        updatePreferencesData();
+        if(username.length()>0 && password.length()>0) {
+            openHome();
+            finish();
+        }
 
 
         button =(Button) findViewById(R.id.buttonAccedi);
@@ -35,12 +46,18 @@ public class Login extends AppCompatActivity {
                  username =  userET.getText().toString();
                  password = passwordET.getText().toString();
                 if (username.contains("@")) {
+
+                savePreferencesData();
                 openHome();
-                Account();
+                //Account();
+                //updatePreferencesData()
                 }
                 else
                     Toast.makeText(Login.this, "@ mancante", Toast.LENGTH_SHORT).show();
             }
+
+
+
         });
 
         button =(Button) findViewById(R.id.buttonReg);
@@ -57,6 +74,7 @@ public class Login extends AppCompatActivity {
         HomeIntent.putExtra("user", username);
         HomeIntent.putExtra("pwd", password);
         startActivity(HomeIntent);
+       // savePreferencesData();
     }
 
     public void openRegistrazione() {
@@ -67,5 +85,35 @@ public class Login extends AppCompatActivity {
         Intent AccountIntent = new Intent(this, Account.class);
         AccountIntent.putExtra("user", username);
         AccountIntent.putExtra("pwd", password);
+    }
+    public  void  savePreferencesData()  {
+
+        SharedPreferences prefs  =  getSharedPreferences(MY_PREFERENCES,  Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor  editor  =  prefs.edit();
+
+        EditText outputUser  =  (EditText)  findViewById(R.id.username);
+        EditText outputPass  =  (EditText)  findViewById(R.id.password);
+        CharSequence  textUser  =  outputUser.getText();
+        CharSequence  textPass  =  outputPass.getText();
+        if  (textUser  !=  null && textPass != null)  {
+
+            editor.putString(TEXT_LOGIN_KEY,  textUser.toString());
+            editor.putString(TEXT_PASSWORD_KEY,  textPass.toString());
+            editor.commit();
+            //openHome();
+        }
+        updatePreferencesData();
+    }
+    private  void  updatePreferencesData(){
+
+        SharedPreferences  prefs  =  getSharedPreferences(MY_PREFERENCES,  Context.MODE_PRIVATE);
+
+        username  =  prefs.getString(TEXT_LOGIN_KEY,  "");
+        password  =  prefs.getString(TEXT_PASSWORD_KEY,  "");
+
+      //  TextView outputView  =  (TextView)  findViewById(R.id.outputData);
+       // outputView.setText(textData);
+        Toast.makeText(Login.this, username, Toast.LENGTH_SHORT).show();
     }
 }
