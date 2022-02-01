7c78b7eb1c3f60b28ff9d17ff1f332e24b4b5b9c
davideeheh <d.cappabianca@itsrizzoli.it>
2022-02-01 16:26:46 +0100

    Implementazione bottoni java

--- gestione_eventi_android/app/src/main/java/com/example/applicazionevera/Assistenza.java
+++ gestione_eventi_android/app/src/main/java/com/example/applicazionevera/Assistenza.java
@@ -2,9 +2,11 @@ package com.example.applicazionevera;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class Assistenza extends AppCompatActivity {
@@ -25,5 +27,54 @@ public class Assistenza extends AppCompatActivity {
     }
     public void messaggio() {
         Toast.makeText(this, "Messaggio Inviato!", Toast.LENGTH_SHORT).show();
-    }
-}
+
+    ImageButton button;
+        button=(ImageButton) findViewById(R.id.toHome);
+                button.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View view) {
+        openHome();
+        }
+        });
+        button=(ImageButton) findViewById(R.id.toSettings);
+        button.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View view) {
+        openSettings();
+        }
+        });
+        button=(ImageButton) findViewById(R.id.toNotifications);
+        button.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View view) {
+        openNotifications();
+        }
+        });
+
+        button=(ImageButton) findViewById(R.id.toSearch);
+        button.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View view) {
+        openSearch();
+        }
+        });
+        }
+
+public void openHome() {
+        Intent HomeIntent = new Intent(this, Home.class);
+        startActivity(HomeIntent);
+        }
+public void openSettings() {
+        Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
+        }
+public void openNotifications() {
+        Intent intent = new Intent(this, Notifiche.class);
+        startActivity(intent);
+        }
+
+public void openSearch() {
+        Intent intent = new Intent(this, Search.class);
+        startActivity(intent);
+        }
+        }
