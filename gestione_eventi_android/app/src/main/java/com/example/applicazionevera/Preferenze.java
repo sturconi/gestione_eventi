4b00b2945f387d1138b40944b07f4c45a79c9e6d
davideeheh <d.cappabianca@itsrizzoli.it>
2022-02-01 16:37:45 +0100

    Aggiunta bottoni classi java

--- gestione_eventi_android/app/src/main/java/com/example/applicazionevera/Preferenze.java
+++ gestione_eventi_android/app/src/main/java/com/example/applicazionevera/Preferenze.java
@@ -2,9 +2,12 @@ package com.example.applicazionevera;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.ImageButton;
 import android.widget.Switch;
 import android.widget.Toast;
 
@@ -19,7 +22,6 @@ public class Preferenze extends AppCompatActivity {
         Switch SwitchInt = (Switch) findViewById(R.id.switchIntr);
         Switch SwitchCul = (Switch) findViewById(R.id.switchCultura);
         Switch SwitchServ = (Switch) findViewById(R.id.switchServizi);
-
 
         SwitchSpo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -68,7 +70,53 @@ public class Preferenze extends AppCompatActivity {
                 }
             }
         });
+        ImageButton button;
+        button=(ImageButton) findViewById(R.id.toHome);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHome();
+            }
+        });
+        button=(ImageButton) findViewById(R.id.toSettings);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSettings();
+            }
+        });
+        button=(ImageButton) findViewById(R.id.toNotifications);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openNotifications();
+            }
+        });
 
+        button=(ImageButton) findViewById(R.id.toSearch);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSearch();
+            }
+        });
     }
 
+    public void openHome() {
+        Intent HomeIntent = new Intent(this, Home.class);
+        startActivity(HomeIntent);
+    }
+    public void openSettings() {
+        Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
+    }
+    public void openNotifications() {
+        Intent intent = new Intent(this, Notifiche.class);
+        startActivity(intent);
+    }
+
+    public void openSearch() {
+        Intent intent = new Intent(this, Search.class);
+        startActivity(intent);
+    }
 }
--- gestione_eventi_android/app/src/main/java/com/example/applicazionevera/Privacy.java
+++ gestione_eventi_android/app/src/main/java/com/example/applicazionevera/Privacy.java
@@ -2,9 +2,12 @@ package com.example.applicazionevera;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.ImageButton;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
@@ -42,7 +45,55 @@ public class Privacy extends AppCompatActivity {
                 }
             }
         });
+        ImageButton button;
+        button=(ImageButton) findViewById(R.id.toHome);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHome();
+            }
+        });
+        button=(ImageButton) findViewById(R.id.toSettings);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSettings();
+            }
+        });
+        button=(ImageButton) findViewById(R.id.toNotifications);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openNotifications();
+            }
+        });
 
+        button=(ImageButton) findViewById(R.id.toSearch);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSearch();
+            }
+        });
+    }
 
+    public void openHome() {
+        Intent HomeIntent = new Intent(this, Home.class);
+        startActivity(HomeIntent);
+    }
+    public void openSettings() {
+        Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
+    }
+    public void openNotifications() {
+        Intent intent = new Intent(this, Notifiche.class);
+        startActivity(intent);
+    }
+
+    public void openSearch() {
+        Intent intent = new Intent(this, Search.class);
+        startActivity(intent);
     }
 }
+
+
