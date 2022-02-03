38b78c61879c3fc1c505fa673533955ea671ee83
davideeheh <d.cappabianca@itsrizzoli.it>
2022-02-03 17:14:33 +0100

    Creazione crea evento + listview

--- gestione_eventi_android/app/src/main/java/com/example/applicazionevera/CreaEvento.java
+++ gestione_eventi_android/app/src/main/java/com/example/applicazionevera/CreaEvento.java
@@ -3,6 +3,8 @@ package com.example.applicazionevera;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class CreaEvento extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class CreaEvento extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crea_evento);
+
+        String[] Categorie = new String[]{"Intrattenimento", "Servizi", "Cultura", "Sport"};
+        ListView cat= (ListView) findViewById(R.id.Listcategorie);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(CreaEvento.this,android.R.layout.simple_list_item_1,Categorie);
+        cat.setAdapter(adapter);
+
+
     }
+
 }
