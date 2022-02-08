f6d0bccf1320ef5fa6af2716eb00e5931558bffc
Kristhen <k.mercoledisanto@itsrizzoli.it>
2022-02-08 17:36:30 +0100

    Aggiunti primi codici a home.js

---
+++ EventApp/src/main/resources/static/home.js
@@ -0,0 +1,68 @@
+var curUtente;
+function requestInfo(UtenteId){
+    var uri= "http://localhost:8080/cani/" +UtenteId;
+    var xmlUtente = new XMLHttpRequest();
+    xmlUtente.onreadystatechange=function(){
+        if(xmlUtente.readyState == 4 && xmlUtente.status == 200)
+        {  
+            //codice di gestione del JSON di ritorno 
+            curUtente = JSON.parse(xmlUtente.responseText);
+            document.getElementById("usernameField").setAttribute("value", curUtente.username);
+            document.getElementById("passwordField").setAttribute("value", curUtente.password);
+
+        }
+    };
+        xmlUtente.open("GET",uri);
+    xmlUtente.send(null);
+}
+function onPageLoaded(){
+    var params = new URLSearchParams(location.search);
+    var nomeVal = params.get("username");
+    /*
+    if (nomeVal!=null) {
+        document.title="Modifica Cane";
+        document.getElementById("descr").innerHTML="Modifica Cane "+nomeVal+"info";
+        requestCaneInfo(parseInt(nomeVal));
+    }
+    else
+    curCane=null;
+}
+function doSubmit()
+{
+    var form = document.getElementById("newForm");
+    
+    var formData = new FormData(document.forms.newForm);
+    var jsonData = JSON.stringify(Object.fromEntries(formData));
+
+    var uriAddr = "http://localhost:8080/cani/";
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function(){
+        if(xmlHttp.readyState == 4)
+        {  
+            if(xmlHttp.status == 201)
+            {
+                if(confirm("CANE CREATO, NE VUOI CREARE UN ALTRO?"))
+                location.reload();
+                else 
+                window.location.replace("http://localhost:8080/home.html");
+
+            }    
+            else if (xmlHttp.status == 200)
+            {
+                alert("CANE MODIFICATO");
+                window.location.replace("http://localhost:8080/home.html");
+            }          
+        }
+    }
+    if(curCane == null)
+    {
+        xmlHttp.open("POST", uriAddr);
+    }
+    else{
+        xmlHttp.open("PUT", uriAddr + "/"+ curCane.nome);
+    }
+    
+    xmlHttp.setRequestHeader("Content-Type", "application/json");
+    xmlHttp.send(jsonData);
+    return false;*/
+}
