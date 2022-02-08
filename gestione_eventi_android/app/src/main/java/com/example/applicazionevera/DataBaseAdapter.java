8e4bb31a0e26a3ab1b4368f6e168e52e56206cd8
davideeheh <d.cappabianca@itsrizzoli.it>
2022-02-08 14:07:48 +0100

    Aggiornamenti DataBaseAdapter

--- gestione_eventi_android/app/src/main/java/com/example/applicazionevera/DataBaseAdapter.java
+++ gestione_eventi_android/app/src/main/java/com/example/applicazionevera/DataBaseAdapter.java
@@ -20,7 +20,7 @@ public class DataBaseAdapter {
     private static final String KEY_codice = "codice";
     private static final String KEY_oggetto_commento = "oggetto_commento";
     private static final String KEY_testo_commento = "testo_commento";
-    private static final String KEY_ID_utente = "ID_utente";
+    private static final String KEY_ID_utente = "_ID_utente";
 
     String DATABASE_TABLE3 = "evento";
     private static final String KEY_numero_evento = "numero_evento";
@@ -33,19 +33,19 @@ public class DataBaseAdapter {
     private static final String KEY_ora_inizio = "ora_inizio";
     private static final String KEY_ora_fine = "ora_fine";
     private static final String KEY_immagine_evento = "immagine_evento";
-    private static final String KEY_ID_posto = "ID_posto";
+    private static final String KEY_ID_posto = "_ID_posto";
 
     String DATABASE_TABLE4 = "evento_utente";
-    private static final String KEY_ID_utente_evento_utente = "ID_utente_evento_utente";
+    private static final String KEY_ID_utente_evento_utente = "_ID_utente_evento_utente";
     private static final String KEY_numero_evento_evento_utente = "numero_evento_evento_utente";
 
     String DATABASE_TABLE5 = "luogo";
-    private static final String KEY_ID_posto_luogo = "ID_posto_luogo";
+    private static final String KEY_ID_posto_luogo = "_ID_posto_luogo";
     private static final String KEY_longitudine = "longitudine";
     private static final String KEY_latitudine = "latitudine";
 
     String DATABASE_TABLE6 = "notifiche";
-    private static final String KEY_ID_notifica = "ID_posto_luogo";
+    private static final String KEY_ID_notifica = "_ID_posto_luogo";
     private static final String KEY_oggetto_notifica= "longitudine";
     private static final String KEY_testo = "latitudine";
     private static final String KEY_data_notifiche = "longitudine";
@@ -55,10 +55,10 @@ public class DataBaseAdapter {
     private static final String KEY_codice_ticket = "codice_ticket";
     private static final String KEY_oggetto_ticket = "oggetto_ticket";
     private static final String KEY_testo_ticket = "testo_ticket";
-    private static final String KEY_ID_utente_ticket = "ID_utente_ticket";
+    private static final String KEY_ID_utente_ticket = "_ID_utente_ticket";
 
     String DATABASE_TABLE8 = "utente";
-    private static final String KEY_ID_utente_utente = "ID_utente_utente";
+    private static final String KEY_ID_utente_utente = "_ID_utente_utente";
     private static final String KEY_nome= "nome";
     private static final String KEY_cognome = "cognome";
     private static final String KEY_username = "username";
