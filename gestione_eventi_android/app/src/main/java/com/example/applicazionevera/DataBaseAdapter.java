06d2d5698ea7dd27e7110b6b21b2720719efb67a
davideeheh <d.cappabianca@itsrizzoli.it>
2022-02-08 14:44:01 +0100

    update 2.0.1 DataBaseAdapter

--- gestione_eventi_android/app/src/main/java/com/example/applicazionevera/DataBaseAdapter.java
+++ gestione_eventi_android/app/src/main/java/com/example/applicazionevera/DataBaseAdapter.java
@@ -1,6 +1,7 @@
 package com.example.applicazionevera;
 
 import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
 public class DataBaseAdapter {
@@ -9,21 +10,20 @@ public class DataBaseAdapter {
     private static final String LOG_TAG = DataBaseAdapter.class.getSimpleName();
     private Context context;
     private SQLiteDatabase database;
-    private DatabaseHelper dbHelper;
-
+    private DataBaseHelper dbHelper;
 
     String DATABASE_TABLE = "categorie";
-    private static final String KEY_Categoria = "Categoria";
+    private static final String KEY_Categoria = "_Categoria";
     private static final String KEY_immagine = "immagine";
 
     String DATABASE_TABLE2 = "commento";
-    private static final String KEY_codice = "codice";
+    private static final String KEY_codice = "_codice";
     private static final String KEY_oggetto_commento = "oggetto_commento";
     private static final String KEY_testo_commento = "testo_commento";
     private static final String KEY_ID_utente = "_ID_utente";
 
     String DATABASE_TABLE3 = "evento";
-    private static final String KEY_numero_evento = "numero_evento";
+    private static final String KEY_numero_evento = "_numero_evento";
     private static final String KEY_Categoria_evento = "Categoria_evento";
     private static final String KEY_nome_evento = "nome_evento";
     private static final String KEY_Descrizione = "Descrizione";
@@ -37,7 +37,7 @@ public class DataBaseAdapter {
 
     String DATABASE_TABLE4 = "evento_utente";
     private static final String KEY_ID_utente_evento_utente = "_ID_utente_evento_utente";
-    private static final String KEY_numero_evento_evento_utente = "numero_evento_evento_utente";
+    private static final String KEY_numero_evento_evento_utente = "_numero_evento_evento_utente";
 
     String DATABASE_TABLE5 = "luogo";
     private static final String KEY_ID_posto_luogo = "_ID_posto_luogo";
@@ -45,28 +45,27 @@ public class DataBaseAdapter {
     private static final String KEY_latitudine = "latitudine";
 
     String DATABASE_TABLE6 = "notifiche";
-    private static final String KEY_ID_notifica = "_ID_posto_luogo";
-    private static final String KEY_oggetto_notifica= "longitudine";
-    private static final String KEY_testo = "latitudine";
-    private static final String KEY_data_notifiche = "longitudine";
-    private static final String KEY_ID_utente_notifiche = "latitudine";
+    private static final String KEY_ID_notifica = "_ID_notifica";
+    private static final String KEY_oggetto_notifica = "oggetto_notifica";
+    private static final String KEY_testo = "testo";
+    private static final String KEY_data_notifiche = "data_notifiche";
+    private static final String KEY_ID_utente_notifiche = "_ID_utente_notifiche";
 
     String DATABASE_TABLE7 = "ticket";
-    private static final String KEY_codice_ticket = "codice_ticket";
+    private static final String KEY_codice_ticket = "_codice_ticket";
     private static final String KEY_oggetto_ticket = "oggetto_ticket";
     private static final String KEY_testo_ticket = "testo_ticket";
     private static final String KEY_ID_utente_ticket = "_ID_utente_ticket";
 
     String DATABASE_TABLE8 = "utente";
     private static final String KEY_ID_utente_utente = "_ID_utente_utente";
-    private static final String KEY_nome= "nome";
+    private static final String KEY_nome = "nome";
     private static final String KEY_cognome = "cognome";
     private static final String KEY_username = "username";
     private static final String KEY_eta = "eta";
-    private static final String KEY_email= "email";
+    private static final String KEY_email = "email";
     private static final String KEY_password = "password";
     private static final String KEY_foto_profilo = "foto_profilo";
-
 
 
     public DataBaseAdapter(Context context) {
@@ -74,10 +73,13 @@ public class DataBaseAdapter {
     }
 
     public DataBaseAdapter open() throws SQLException {
-        dbHelper = new DatabaseHelper(context);
+        dbHelper = new DataBaseHelper(context);
         database = dbHelper.getWritableDatabase();
         return this;
     }
+
     public void close() {
         dbHelper.close();
+    }
 }
+
