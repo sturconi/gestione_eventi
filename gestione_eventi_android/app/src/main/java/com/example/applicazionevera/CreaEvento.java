4d9d770999f5d2a3aba29940e80ffe6b8c688eaf
app eventi <s.turconi@itsrizzoli.it>
2022-02-08 15:03:28 +0100

    Modificata activity_crea_evento.xml

--- gestione_eventi_android/app/src/main/java/com/example/applicazionevera/CreaEvento.java
+++ gestione_eventi_android/app/src/main/java/com/example/applicazionevera/CreaEvento.java
@@ -2,9 +2,13 @@ package com.example.applicazionevera;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class CreaEvento extends AppCompatActivity {
 
@@ -14,11 +18,23 @@ public class CreaEvento extends AppCompatActivity {
         setContentView(R.layout.activity_crea_evento);
 
         String[] Categorie = new String[]{"Intrattenimento", "Servizi", "Cultura", "Sport"};
-        ListView cat= (ListView) findViewById(R.id.Listcategorie);
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(CreaEvento.this,android.R.layout.simple_list_item_1,Categorie);
+        ListView cat = (ListView) findViewById(R.id.Listcategorie);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(CreaEvento.this, android.R.layout.simple_list_item_1, Categorie);
         cat.setAdapter(adapter);
 
-
+        Button button;
+        button = (Button) findViewById(R.id.pagSucc);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPagSucc();
+                Toast.makeText(CreaEvento.this, "hai premuto pagina successiva", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
+    public void openPagSucc() {
+        Intent intent = new Intent(this, CreaEvento2.class);
+        startActivity(intent);
+    }
 }
