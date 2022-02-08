0e7dfdf10ed6c93403cb89ffda1bae5d86fb9314
FrancoCeruti <94625923+FrancoCeruti@users.noreply.github.com>
2022-02-08 17:37:45 +0100

    Aggiunto utenteRestController

---
+++ EventApp/src/main/java/ese/example/Ese/UtenteRestController.java
@@ -0,0 +1,76 @@
+package ese.example.Ese;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UtenteRestController {
+	
+	@Autowired
+	utenteInterface utenteRepositery;
+	
+	@RequestMapping(value="/utenti",method=RequestMethod.GET)
+	public List<Utente> getUtente(){
+		return this.utenteRepositery.findAll();
+		
+		
+	}
+	
+	@RequestMapping(value="/utente/{id}", method=RequestMethod.GET)
+	public ResponseEntity<Utente> getUtenteByid(@PathVariable int id) {
+		Utente u =this.utenteRepositery.findById(id);
+		if(u!=null) {
+		return new ResponseEntity(u,HttpStatus.OK);
+		}
+		else {
+			return new ResponseEntity(u,HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@RequestMapping(value="/utenti/{id}", method=RequestMethod.DELETE)
+	public void deleteAllUtenti() {
+		this.utenteRepositery.deleteAll();
+	 }
+	
+	
+	@RequestMapping(value="/utente", method=RequestMethod.POST)
+	public ResponseEntity<String> addUtente(@RequestBody Utente newUtente) {
+		String res="";
+		if(this.utenteRepositery.save(newUtente)) {
+			return new ResponseEntity<String>("Ok", HttpStatus.CREATED);
+		}
+		else {
+			return new ResponseEntity<String>("KO", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	
+	}
+	
+
+	@RequestMapping(value="/utente/{id}", method=RequestMethod.DELETE)
+	public void deleteUtenteByid(@PathVariable int id) {
+		this.utenteRepositery.deleteById(id);
+	}
+	
+	
+	@RequestMapping(value="/utente/{id}", method=RequestMethod.PUT)
+	public Utente updateUtente(@PathVariable int id,@RequestBody Utente newUtente) {
+	Utente u=this.utenteRepositery.findById(id);
+	if((!newUtente.getNome().isBlank())&& newUtente.getNome() !=null) {
+		u.setNome(newUtente.getNome());
+	}
+	
+	this.utenteRepositery.update(u);
+	return u;
+	}
+}
+
