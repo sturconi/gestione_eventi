6dfb0ba69ff8661651fddbed17d01bc7dba3ad91
davideeheh <d.cappabianca@itsrizzoli.it>
2022-02-09 17:14:35 +0100

    metodi openEventoDettagliato

--- gestione_eventi_android/app/src/main/java/com/example/applicazionevera/Evento.java
+++ gestione_eventi_android/app/src/main/java/com/example/applicazionevera/Evento.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class Evento extends AppCompatActivity {
@@ -14,7 +15,7 @@ public class Evento extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_evento);
 
-
+        Button bottone;
         ImageButton button;
         button=(ImageButton) findViewById(R.id.toHome);
         button.setOnClickListener(new View.OnClickListener() {
@@ -45,6 +46,38 @@ public class Evento extends AppCompatActivity {
                 openSearch();
             }
         });
+        bottone=(Button)findViewById(R.id.button24);
+        bottone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openEventoDettagliato();
+            }
+
+        });
+        bottone=(Button)findViewById(R.id.button27);
+        bottone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openEventoDettagliato();
+            }
+
+        });
+        bottone=(Button)findViewById(R.id.button26);
+        bottone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openEventoDettagliato();
+            }
+
+        });
+        bottone=(Button)findViewById(R.id.button25);
+        bottone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openEventoDettagliato();
+            }
+
+        });
     }
 
     public void openHome() {
@@ -62,6 +95,10 @@ public class Evento extends AppCompatActivity {
 
     public void openSearch() {
         Intent intent = new Intent(this, Search.class);
+        startActivity(intent);
+    }
+    public void openEventoDettagliato() {
+        Intent intent = new Intent(this, EventoDettagliato.class);
         startActivity(intent);
     }
 }
