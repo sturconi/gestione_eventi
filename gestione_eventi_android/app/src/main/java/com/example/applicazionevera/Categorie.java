fdd39c91cfa29339fc6461a372e106616691f567
MatteoRomagnoni <94623792+MatteoRomagnoni@users.noreply.github.com>
2022-02-10 09:29:35 +0100

    Sistemata navigazione categorie, evento, evento dett.

--- gestione_eventi_android/app/src/main/java/com/example/applicazionevera/Categorie.java
+++ gestione_eventi_android/app/src/main/java/com/example/applicazionevera/Categorie.java
@@ -1,12 +1,16 @@
 package com.example.applicazionevera;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Categorie extends AppCompatActivity {
 
@@ -14,6 +18,31 @@ public class Categorie extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categorie);
+
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.toHome:
+                        openHome();
+                        break;
+                    case R.id.toSearch:
+                        openSearch();
+                        break;
+                    case R.id.toNotifications:
+                        openNotifications();
+                        break;
+                    case R.id.toSettings:
+                        openSettings();
+                        break;
+                }
+                return false;
+            }
+        });
+
+
         Button bottone;
         ImageButton button;
 
--- gestione_eventi_android/app/src/main/java/com/example/applicazionevera/Evento.java
+++ gestione_eventi_android/app/src/main/java/com/example/applicazionevera/Evento.java
@@ -1,19 +1,47 @@
 package com.example.applicazionevera;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class Evento extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_evento);
+
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.toHome:
+                        openHome();
+                        break;
+                    case R.id.toSearch:
+                        openSearch();
+                        break;
+                    case R.id.toNotifications:
+                        openNotifications();
+                        break;
+                    case R.id.toSettings:
+                        openSettings();
+                        break;
+                }
+                return false;
+            }
+        });
 
         Button bottone;
         ImageButton button;
