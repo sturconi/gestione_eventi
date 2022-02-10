729c4d7d1428b124bf45aba25978eb22203540e6
FrancoCeruti <94625923+FrancoCeruti@users.noreply.github.com>
2022-02-10 15:00:56 +0100

    Update registrazione

--- EventApp/src/main/resources/static/registrazione.js
+++ EventApp/src/main/resources/static/registrazione.js
@@ -1,38 +1,8 @@
-var curUtente;
-
-function requestUtenteinfo(id){
-    var uriAdd= "./utente/"+id;
-    var request= new XMLHttpRequest();
-    request.onreadystatechange = function(){
-    if(request.readyState == 4 && request.status == 200)
-    {
-        curUtente=JSON.parse(request.responseText)
-        document.getElementById("nome").setAttribute("value", curUtente.nome)
-        document.getElementById("id").setAttribute("value", curUtente.id)
-        document.getElementById("cognome").setAttribute("value", curUtente.cognome)
-        document.getElementById("username").setAttribute("value", curUtente.username)
-        document.getElementById("email").setAttribute("value", curUtente.email)
-        document.getElementById("password").setAttribute("value", curUtente.password)
-        document.getElementById("data").setAttribute("value", curUtente.data)
-        }
-};
-request.open('GET', uriAdd);
-request.send(null);
-}
-
-function onPageLoaded(){
-    var params= new URLSearchParams(location.search);
-    var idval=params.get("id");
-    if(idval!=null ){
-     requestUtenteinfo(parseInt(idval))
-    }
-    else{
-        curUtente=null;
-    }
-}
 
 function doSubmit()
 {   
+    if ((document.getElementById("password").value)==(document.getElementById("passwordConf").value)){
+        
     var formData = new FormData(document.forms.formRegister);
     var jsonData = JSON.stringify(Object.fromEntries(formData));
 
@@ -48,11 +18,18 @@ function doSubmit()
             }          
         }
     }
-    if(curUtente == null)
-    {
-        request.open("POST", uriAddr);
-    }
+    
+    request.open("POST", uriAddr);
     request.setRequestHeader("Content-Type", "application/json");
     request.send(jsonData);
     return false;
+
+    }
+    else{
+        alert("Le password non coincidono!");
+        document.getElementById("passwordConf").focus();
+        return(false);
+    }
+
+
 }
