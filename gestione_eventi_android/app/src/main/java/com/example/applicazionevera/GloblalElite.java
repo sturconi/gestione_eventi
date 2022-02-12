4154328bd786873bfd5720e06092f7af10c873be
Kristhen <k.mercoledisanto@itsrizzoli.it>
2022-02-12 15:09:02 +0100

    Creata classe GloblalElite

---
+++ gestione_eventi_android/app/src/main/java/com/example/applicazionevera/GloblalElite.java
@@ -0,0 +1,14 @@
+package com.example.applicazionevera;
+
+import android.app.Application;
+
+public class GloblalElite extends Application {
+    boolean switchButton;
+    public GloblalElite(){}
+    public void onCreate() {
+
+        super.onCreate();
+
+        
+    }
+}
