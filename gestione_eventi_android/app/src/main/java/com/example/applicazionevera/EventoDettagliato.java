458d3bd18b22948221dd98b44c454dbd6210f97c
app eventi <s.turconi@itsrizzoli.it>
2022-02-18 19:25:03 +0100

    Aggiunto pezzo di codice mancante

--- gestione_eventi_android/app/src/main/java/com/example/applicazionevera/EventoDettagliato.java
+++ gestione_eventi_android/app/src/main/java/com/example/applicazionevera/EventoDettagliato.java
@@ -1,8 +1,13 @@
 package com.example.applicazionevera;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,6 +16,9 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.applicazionevera.databinding.ActivityMappaBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Calendar;
 
 public class EventoDettagliato extends FragmentActivity implements OnMapReadyCallback {
 
@@ -28,6 +36,89 @@ public class EventoDettagliato extends FragmentActivity implements OnMapReadyCal
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.toHome:
+                        openHome();
+                        overridePendingTransition(0,0);
+                        break;
+                    case R.id.toSearch:
+                        openSearch();
+                        overridePendingTransition(0,0);
+                        break;
+                    case R.id.toNotifications:
+                        openNotifications();
+                        overridePendingTransition(0,0);
+                        break;
+                    case R.id.toSettings:
+                        openSettings();
+                        overridePendingTransition(0,0);
+                        break;
+                }
+                return false;
+            }
+        });
+        Button bottone;
+        Button but;
+
+        bottone = (Button) findViewById(R.id.sezioneCommenti);
+        bottone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSezioneComm();
+            }
+        });
+
+
+        bottone =(Button) findViewById(R.id.toCalendario);
+        bottone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Calendar cal = Calendar.getInstance();
+                Intent intent = new Intent(Intent.ACTION_EDIT);
+                intent.setType("vnd.android.cursor.item/event");
+                intent.putExtra("beginTime", cal.getTimeInMillis());
+                intent.putExtra("allDay", true);
+                intent.putExtra("rrule", "FREQ=YEARLY");
+                intent.putExtra("endTime", cal.getTimeInMillis()+60*60*1000);
+                intent.putExtra("title", "Sei stato eventato ;-)" );
+                startActivity(intent);
+
+            }
+        });
+
+    }
+
+    private void openSezioneComm() {
+        Intent intent = new Intent(this, SezioneCommenti.class);
+        startActivity(intent);
+    }
+
+    public void openHome() {
+        Intent HomeIntent = new Intent(this, Home.class);
+        startActivity(HomeIntent);
+    }
+    public void openSettings() {
+        Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
+    }
+    public void openNotifications() {
+        Intent intent = new Intent(this, Notifiche.class);
+        startActivity(intent);
+    }
+
+    public void openSearch() {
+        Intent intent = new Intent(this, Search.class);
+        startActivity(intent);
+    }
+    public void openCalendario() {
+        Intent intent = new Intent(this, Calendario.class);
+        startActivity(intent);
     }
 
     @Override
