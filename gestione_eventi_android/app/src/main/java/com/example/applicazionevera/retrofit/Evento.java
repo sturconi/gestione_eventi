ec05717821a9dc41ac2c3196ad31658d795078c4
FrancoCeruti <94625923+FrancoCeruti@users.noreply.github.com>
2022-02-23 10:58:54 +0100

    Creata classe evento

---
+++ gestione_eventi_android/app/src/main/java/com/example/applicazionevera/retrofit/Evento.java
@@ -0,0 +1,100 @@
+package com.example.applicazionevera.retrofit;
+
+
+import java.sql.Date;
+
+public class Evento {
+        int numero_evento;
+        String Categoria;
+        String nome_evento;
+        String Descrizione;
+        int Valutazione;
+        String luogo;
+        Date data;
+        Date oraInizio;
+        Date oraFine;
+        String immagine;
+        int ID_Posto;
+
+        public Evento() {
+        }
+
+        public Evento(int numero_evento, String Categoria, String nome_evento, String Descrizione, int Valutazione, String luogo, Date data, Date oraInizio, Date oraFine, String immagine, int ID_Posto) {
+            this.numero_evento= numero_evento;
+            this.Categoria= Categoria;
+            this.nome_evento= nome_evento;
+            this.Descrizione=Descrizione;
+            this.Valutazione=Valutazione;
+            this.luogo=luogo;
+            this.data=data;
+            this.oraInizio=oraInizio;
+            this.oraFine=oraFine;
+            this.immagine=immagine;
+            this.ID_Posto=ID_Posto;
+        }
+        public int getnumero_evento() {
+            return this.numero_evento;
+        }
+        public String getCategoria() {
+            return this.Categoria;
+        }
+        public String getNome_evento() {
+            return this.nome_evento;
+        }
+        public String getDescrizione() {
+            return this.Descrizione;
+        }
+        public int getValutazione() {
+            return this.Valutazione;
+        }
+        public String getLuogo() {
+            return this.luogo;
+        }
+        public Date getData() {
+            return this.data;
+        }
+        public Date getOraInizio() {
+            return this.oraInizio;
+        }
+        public Date getOraFine() {
+            return this.oraFine;
+        }
+        public String getImmagine() {
+            return this.immagine;
+        }
+
+        public void setCategoria(String Categoria) {
+            this.Categoria= Categoria;
+        }
+        public void setnumero_Evento(int nnumero_evento) {
+            this.numero_evento=numero_evento;
+        }
+        public void setNome_evento(String nome_evento) {
+            this.nome_evento= nome_evento;
+        }
+        public void setDescrizione(String Descrizione) {
+            this.Descrizione=Descrizione;
+        }
+        public void setValutazione(int Valutazione) {
+            this.Valutazione=Valutazione;
+        }
+        public void setLuogo(String luogo) {
+            this.luogo=luogo;
+        }
+        public void setData(Date data) {
+            this.data=data;
+        }
+        public void setOraInizio(Date oraInizio) {
+            this.oraInizio=oraInizio;
+        }
+        public void setOraFine(Date oraFine) {
+            this.oraFine=oraFine;
+        }
+        public void setImmagine(String immagine) {
+            this.immagine=immagine;
+        }
+        public void setID_Posto(int ID_Posto) {
+            this.ID_Posto=ID_Posto;
+        }
+
+    }
