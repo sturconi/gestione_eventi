85262128e905b222d95a079715b8e373dc6131fa
MatteoRomagnoni <94623792+MatteoRomagnoni@users.noreply.github.com>
2022-03-04 20:22:58 +0100

    search collegato a database e fixato problema CORS

--- EventApp/src/main/resources/static/search.js
+++ EventApp/src/main/resources/static/search.js
@@ -5,22 +5,13 @@ function onPageLoaded(){
 
 
     $(function () {
-        $(".card-container:first").hide()
+        $(".card:first").hide()
         $.ajax({
             url: "http://localhost:8080/eventi",
-            headers: {
-                'Content-Type': 'http://127.0.0.1:5500'
-            },
-            type: "GET", /* or type:"GET" or type:"PUT" */
-            dataType: "json",
-            data: {
-            },
-
-        
-
+            
             success: function(result) {
                 $.each(result, function(index, item) {
-                  var cards = $(".card-container:first").clone() //clone first divs
+                  var cards = $(".card:first").clone() //clone first divs
 
                   console.log("sium");
                   
@@ -32,14 +23,14 @@ function onPageLoaded(){
                   var descev = item.descrizione;
                   //add values inside divs
                 //  $(cards).find(".card-header").html("user id: " + userId + " - " + "id: " + typeId);
-                  $(cards).find(".card-title").html(nome);
-                 // $(cards).find(".event--img").html(immag);
-                 // $(cards).find(".orario-evento").html(oraev);
-                  $(cards).find(".indirizzo-evento").html(indev);
+                  $(cards).find(".titolo_evento").html(nome);
+                  $(cards).find(".card__image").html(immag);
                   $(cards).find(".data-evento").html(dataev);
-                  $(cards).find(".descrizione-evento").html(descev);
+                  $(cards).find(".indirizzo-evento").html(indev);
+                  $(cards).find(".orario-evento").html(oraev);
+                  $(cards).find(".desc_evento").html(descev);
                   $(cards).show() //show cards
-                  $(cards).appendTo($(".portfolio-container ")) 
+                  $(cards).appendTo($(".container ")) 
                  
                 });
               }
