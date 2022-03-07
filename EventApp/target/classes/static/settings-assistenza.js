7f7b5a29202714361dfd92a48401411da5b0521f
Kristhen <k.mercoledisanto@itsrizzoli.it>
2022-03-07 15:57:03 +0100

    collegamenti nei settings

--- EventApp/target/classes/static/settings-assistenza.js
+++ EventApp/target/classes/static/settings-assistenza.js
@@ -1,13 +1,27 @@
-function sendEmail() {
-	Email.send({
-	Host: "smtp.gmail.com",
-	Username : "<mercoledisanto.kristhen@gmail.com>",
-	Password : "<56675024>",
-	To : '<k.mercoledisanto@itsrizzoli.it>',
-	From : "<mercoledisanto.kristhen@gmail.com>",
-	Subject : "<fff>",
-	Body : "<fff>",
-	}).then(
-		message => alert("mail sent successfully")
-	);
+function sendEmail() {   
+   
+        
+    var formData = new FormData(document.forms.formRegister);
+    var jsonData = JSON.stringify(Object.fromEntries(formData));
+
+    var uriAddr = "./ticket";
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState == 4)
+        {  
+            if(request.status == 201)
+            {
+                window.location.replace("./login.html");
+
+            }          
+        }
+    }
+    
+    request.open("POST", uriAddr);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.send(jsonData);
+    return false;
+
+   
+
 }
