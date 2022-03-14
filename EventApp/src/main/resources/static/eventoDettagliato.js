66d7e3755d608f821d182171039fa53ad9e050e4
MatteoRomagnoni <94623792+MatteoRomagnoni@users.noreply.github.com>
2022-03-14 11:41:15 +0100

    commenti js collegato meglio al database

--- EventApp/src/main/resources/static/eventoDettagliato.js
+++ EventApp/src/main/resources/static/eventoDettagliato.js
@@ -17,15 +17,61 @@ var maincomment = function() {
 
 
 
+
+  /* carica evento dettagliato */
+
+  function onEventLoaded(){
+    console.log("PAGINA CARICATA");
+    
+
+
+
+    $(function () {
+        $(".card:first").hide()
+        $.ajax({
+            url: "http://localhost:8080/eventi",
+            
+            success: function(result) {
+                $.each(result, function(index, item) {
+                  var cards = $(".card:first").clone() //clone first divs
+
+                  console.log("sium");
+                  
+                  var nome = item.nome_evento;
+                  var immag = item.immagine;
+                  var indev = item.luogo;
+                  var oraev = item.ora;
+                  var dataev = item.data;
+                  var descev = item.descrizione;
+                  //add values inside divs
+                //  $(cards).find(".card-header").html("user id: " + userId + " - " + "id: " + typeId);
+                  $(cards).find(".titolo_evento").html(nome);
+                  $(cards).find(".card__image").html(immag);
+                  $(cards).find(".data_evento").html(dataev);
+                  $(cards).find(".indirizzo-evento").html(indev);
+                  $(cards).find(".orario-evento").html(oraev);
+                  $(cards).find(".desc_evento").html(descev);
+                  $(cards).show() //show cards
+                  $(cards).appendTo($(".container ")) 
+                 
+                });
+              }
+            });
+          });
+
+}
+
+
   /* carica commenti */
   function onPageLoaded(){
     console.log("PAGINA CARICATA");
 
+    var num = 33;
 
     $(function () {
         $(".notifications:first").hide()
         $.ajax({
-            url: "http://localhost:8080/commenti/11",
+            url: "http://localhost:8080/commenti/" + num,
             
             success: function(result) {
                 $.each(result, function(index, item) {
